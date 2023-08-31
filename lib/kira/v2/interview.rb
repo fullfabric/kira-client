b641e96d6f88c97ac286f863a6241420fa59f9eb
João Amaral <j_amaral@live.com.pt>
2023-08-31 16:28:00 +0100

    fixes on webhook creation

--- lib/kira/v2/interview.rb
+++ lib/kira/v2/interview.rb
@@ -3,21 +3,37 @@ class Kira::V2::Interview
 
   BASE_URL = 'https://app.kiratalent.com/api'
 
-  Contract String, String => Object
-  def initialize(interview_id, token)
-    @interview_id, @token = interview_id, token
+  Contract String, String, String => Object
+  def initialize(interview_id, token, secret)
+    @interview_id, @token, @secret = interview_id, token, secret
   end
 
   Contract Hash => Hash
   def create_webhook
     url = "#{BASE_URL}/interviews/#{@interview_id}/webhooks/"
 
+    # check first if a webhook for this interview already exists
+    res = conn.get do |req|
+      req.url url
+      req.headers["Authorization"] = " Token #{@token}"
+      req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
+      req.headers["Content-Type"] = "application/json"
+    end
+
+    response = JSON.parse(res.body)
+    handle_error(response) unless res.success?
+
+    # return if a webhook already exists
+    return if response.any? { |webhook| webhook['event_subscriptions'].include?('applicant.interview_completed') }
+
+    # create a webhook otherwise
     request_body = {
       "endpoint": "#{::Core::Utils::Host.build.to_s}/api/applics/kira/callback",
       "active": true,
       "event_subscriptions": [
         "applicant.interview_completed"
-      ]
+      ],
+      "secret": @secret
     }
 
     res = conn.post do |req|
