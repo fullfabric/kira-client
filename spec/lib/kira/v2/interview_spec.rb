97b25e1d08170486763ad43620ab6e2a4fc3705f
João Amaral <j_amaral@live.com.pt>
2023-09-01 11:49:20 +0100

    tests and refactoring

--- spec/lib/kira/v2/interview_spec.rb
+++ spec/lib/kira/v2/interview_spec.rb
@@ -1,43 +1,33 @@
 describe Kira::V2::Interview do
 
   BASE_URL = 'https://app.kiratalent.com/api'
+  INTERVIEW_ID = 'fqvjnY'
 
   context 'interview' do
 
     let ( :conn ) { Faraday.new(BASE_URL) }
-    let ( :interview_id ) { "fqvjnY" }
+    let ( :url ) { "#{BASE_URL}/interviews/#{INTERVIEW_ID}/webhooks/" }
     let ( :token ) { "65a8412c301ed85974f03aa71f15ed107ce09786" }
     let ( :secret ) { "x49qwXDyAZDZTdizkLGg" }
-
     let ( :endpoint ) { "https://fullfabric.com/api/applics/kira/callback" }
     let ( :event_subscriptions ) { ["applicant.interview_completed"] }
 
-    let!( :service ) { Kira::V2::Interview.new(interview_id, token, secret) }
+    let!( :service ) { Kira::V2::Interview.new(INTERVIEW_ID, token, secret) }
+
+    after(:each) do
+      _delete_existing_interview_webhooks!
+    end
 
     context 'creating a webhook' do
 
       context 'webhook exists' do
 
         before do
-          url = "#{BASE_URL}/interviews/#{interview_id}/webhooks/"
-
-          request_body = {
-            "endpoint": endpoint,
-            "active": true,
-            "event_subscriptions": event_subscriptions,
-            "secret": secret
-          }
-
-          conn.post do |req|
-            req.url url
-            req.headers["Authorization"] = " Token #{token}"
-            req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
-            req.headers["Content-Type"] = "application/json"
-            req.body = request_body.to_json
-          end
+          _create_interview_webhook!
         end
 
         it 'returns true when webhook exists' do
+          expect(_get_existing_interview_webhooks).not_to be_empty
           expect(
             service.create(
               endpoint: endpoint,
@@ -51,38 +41,18 @@ describe Kira::V2::Interview do
       context 'webhook does not exist' do
 
         before do
-          url = "#{BASE_URL}/interviews/#{interview_id}/webhooks/"
-
-          res = conn.get do |req|
-            req.url url
-            req.headers["Authorization"] = " Token #{token}"
-            req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
-            req.headers["Content-Type"] = "application/json"
-          end
-
-          webhooks = JSON.parse(res.body)
-
-          webhooks.each do |webhook|
-            webhook_url = url + "#{webhook['uid']}/"
-            conn.delete do |req|
-              req.url webhook_url
-              req.headers["Authorization"] = " Token #{token}"
-              req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
-              req.headers["Content-Type"] = "application/json"
-            end
-
-          end
-
+          _delete_existing_interview_webhooks!
         end
 
-        it 'returns an hash with webhook details' do
+        it 'creates a new webhook and returns webhook details' do
+          expect(_get_existing_interview_webhooks).to be_empty
+
           webhook = service.create(
             endpoint: endpoint,
             event_subscriptions: event_subscriptions
           )
 
           expect(webhook).to be_a(Hash)
-
           expect(webhook['uid']).not_to be_empty
           expect(webhook['endpoint']).to eq(endpoint)
           expect(webhook['event_subscriptions']).to eq(event_subscriptions)
@@ -93,4 +63,50 @@ describe Kira::V2::Interview do
     end
 
   end
+
 end
+
+private
+
+  def _create_interview_webhook!
+    request_body = {
+      "endpoint": endpoint,
+      "active": true,
+      "event_subscriptions": event_subscriptions,
+      "secret": secret
+    }
+
+    conn.post do |req|
+      req.url url
+      req.headers["Authorization"] = " Token #{token}"
+      req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
+      req.headers["Content-Type"] = "application/json"
+      req.body = request_body.to_json
+    end
+  end
+
+  def _get_existing_interview_webhooks
+    res = conn.get do |req|
+      req.url url
+      req.headers["Authorization"] = " Token #{token}"
+      req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
+      req.headers["Content-Type"] = "application/json"
+    end
+
+    webhooks = JSON.parse(res.body)
+    webhooks
+  end
+
+  def _delete_existing_interview_webhooks!
+    webhooks = _get_existing_interview_webhooks
+
+    webhooks.each do |webhook|
+      webhook_url = url + "#{webhook['uid']}/"
+      conn.delete do |req|
+        req.url webhook_url
+        req.headers["Authorization"] = " Token #{token}"
+        req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
+        req.headers["Content-Type"] = "application/json"
+      end
+    end
+  end
