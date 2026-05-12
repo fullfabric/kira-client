4c7ad384ce59bc0b508f6d713f55669b96006eca
João Ferreira <joao.ferreira@fullfabric.com>
2026-05-12 14:57:12 +0100

    SQ2-1050 - Harden error handling and DRY shared Faraday plumbing

--- lib/kira/v2/applicant.rb
+++ lib/kira/v2/applicant.rb
@@ -1,7 +1,6 @@
 class Kira::V2::Applicant
   include Contracts
-
-  BASE_URL = 'https://app.kiratalent.com/api'
+  include Kira::V2::Client
 
   Contract String, String => Any
   def initialize(interview_id, token)
@@ -10,29 +9,6 @@ class Kira::V2::Applicant
 
   Contract Hash => Hash
   def create(applicant)
-
-    url = "#{BASE_URL}/interviews/#{@interview_id}/applicants/"
-
-    res = conn.post do |req|
-      req.url url
-      req.headers["Authorization"] = " Token #{@token}"
-      req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
-      req.headers["Content-Type"] = "application/json"
-      req.body = applicant.to_json
-    end
-
-    response = JSON.parse(res.body)
-    res.success? ? response : handle_error(response)
+    request(:post, "/interviews/#{@interview_id}/applicants/", body: applicant)
   end
-
-  private
-
-    def conn
-      @conn ||= Faraday.new(BASE_URL)
-    end
-
-    def handle_error(response)
-      raise Kira::Error.new(response)
-    end
-
 end
--- lib/kira/v2/interview.rb
+++ lib/kira/v2/interview.rb
@@ -1,7 +1,6 @@
 class Kira::V2::Interview
   include Contracts
-
-  BASE_URL = 'https://app.kiratalent.com/api'
+  include Kira::V2::Client
 
   Contract String, String, String => Any
   def initialize(interview_id, token, secret)
@@ -14,50 +13,16 @@ class Kira::V2::Interview
     active: Optional[Bool]
   ] => Or[Hash, Bool]
   def create(endpoint:, event_subscriptions:, active: true)
-    url = "#{BASE_URL}/interviews/#{@interview_id}/webhooks/"
+    path = "/interviews/#{@interview_id}/webhooks/"
 
-    # check first if a webhook for this interview already exists
-    res = conn.get do |req|
-      req.url url
-      req.headers["Authorization"] = " Token #{@token}"
-      req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
-      req.headers["Content-Type"] = "application/json"
-    end
+    existing = request(:get, path)
+    return true if existing.any? { |webhook| webhook['event_subscriptions'].include?('applicant.interview_completed') }
 
-    response = JSON.parse(res.body)
-    handle_error(response) unless res.success?
-
-    # return if a webhook already exists
-    return true if response.any? { |webhook| webhook['event_subscriptions'].include?('applicant.interview_completed') }
-
-    # create a webhook otherwise
-    request_body = {
-      "endpoint": endpoint,
-      "event_subscriptions": event_subscriptions,
-      "active": active,
-      "secret": @secret
-    }
-
-    res = conn.post do |req|
-      req.url url
-      req.headers["Authorization"] = " Token #{@token}"
-      req.headers["Accept"] = "application/vnd.kiratalent.v2+json"
-      req.headers["Content-Type"] = "application/json"
-      req.body = request_body.to_json
-    end
-
-    response = JSON.parse(res.body)
-    res.success? ? response : handle_error(response)
+    request(:post, path, body: {
+      endpoint: endpoint,
+      event_subscriptions: event_subscriptions,
+      active: active,
+      secret: @secret
+    })
   end
-
-  private
-
-    def conn
-      @conn ||= Faraday.new(BASE_URL)
-    end
-
-    def handle_error(response)
-      raise Kira::Error.new(response)
-    end
-
 end
